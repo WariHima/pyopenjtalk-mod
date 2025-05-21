b8a6811b899060c1eab2fefe7283a8c4b901f29a
WariHima <simamumin02@gmail.com>
2025-05-21 09:38:44 +0900

    update version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import Extension, find_packages, setup
 
 platform_is_windows = sys.platform == "win32"
 
-version = "0.4.1.31"
+version = "0.4.1.32"
 
 msvc_extra_compile_args_config = [
     "/source-charset:utf-8",
