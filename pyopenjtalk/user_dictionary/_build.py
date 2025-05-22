f8b53dc76f305337f6d1e4e3ce20f6f2ca4dac86
WariHima <simamumin02@gmail.com>
2025-05-22 12:44:22 +0900

    辞書の追加

--- pyopenjtalk/user_dictionary/_build.py
+++ pyopenjtalk/user_dictionary/_build.py
@@ -7,7 +7,7 @@ def is_csv_file(file: Path) -> bool:
     supported_extensions = [".csv"]
     return file.suffix.lower() in supported_extensions
 
-input_dir_path = Path("pyopenjtalk/user_dictionary")
+input_dir_path = Path("pyopenjtalk/user_dict_src")
 csv_files = [file for file in input_dir_path.rglob("*") if is_csv_file(file)]
 
 for file in csv_files:
